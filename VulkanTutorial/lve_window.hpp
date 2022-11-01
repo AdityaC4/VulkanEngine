568aaf47c3c79d822868b25b30f702c89f74597e
AdityaC4 <adityapchaudhari@gmail.com>
2022-11-01 12:26:46 -0400

    device setup | pipeline configuration

--- VulkanTutorial/lve_window.hpp
+++ VulkanTutorial/lve_window.hpp
@@ -16,6 +16,9 @@ namespace lve {
 		LveWindow& operator=(const LveWindow&) = delete;
 
 		bool shouldClose() { return glfwWindowShouldClose(window); }
+
+		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
+
 	private:
 
 		void initWindow();
